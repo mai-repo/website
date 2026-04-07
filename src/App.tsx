82c43058489dca4ddb452fe25e2c7c6a796c67e6
Thanh Mai <thanhmai@mac.mynetworksettings.com>
2026-04-06 20:25:51 -0400

    Remove basename

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import ScrollToTop from "@/components/ScrollToTop";
 
 export default function App() {
   return (
-    <Router basename="/website">
+    <Router>
       <ScrollToTop />
       <div className="min-h-screen selection:bg-brand-green selection:text-brand-bg flex flex-col">
         <Header />
